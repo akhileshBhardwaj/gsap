0f65e0e36d48fcad0d944bec4eca9109d11fbe3e
akhileshBhardwaj <rajbharakhilesh818@gmail.com>
2026-08-02 11:10:18 +0530

    update

---
+++ Stunning_svg_Animation_vid_3/script.js
@@ -0,0 +1,8 @@
+var path = "M 10 100 Q 500 100 990 100";
+var finalPath = "M 10 100 Q 500 100 990 100";
+
+const string = document.querySelector("#string");
+
+string.addEventListener("mouseenter", function () {
+  console.log("Mouse Entered");
+});
